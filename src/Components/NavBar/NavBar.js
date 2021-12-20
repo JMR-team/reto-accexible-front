4ee81e910205f0c1008a36e39456665fc751e499
Miguel-ASM <miguel.a.s.martinez@gmail.com>
2021-12-20 13:55:09 +0100

    Distintos enlaces en la navbar si logeado o no

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
-import { NavLink, Link } from "react-router-dom";
+import React, { useState , useEffect} from "react";
+import { NavLink, Link, useNavigate } from "react-router-dom";
 import { MenuList } from "./MenuList";
 import "./NavBar.css";
 
 export default function Navbar (){
+  const navigate = useNavigate();
+  const [userIsLogged,setUserIsLogged] = useState( localStorage.getItem('token') !=null )
   const [clicked, setClicked] = useState(false);
-  const menuList = MenuList.map(({ url, title }, index) => {
+  const menuList = MenuList.filter(({showAlways})=>showAlways || !userIsLogged).map(({ url, title }, index) => {
     return (
       <li key={index}>
         <NavLink exact to={url} activeClassName="active">
@@ -15,21 +17,41 @@ export default function Navbar (){
     );
   });
 
+  useEffect(() => {
+    if (localStorage.getItem("token") == null) {
+      setUserIsLogged(false);
+    } else {
+      setUserIsLogged(true);
+    }
+  }, [localStorage.getItem("token")]);
+
+
   const handleClick = () => {
     setClicked(!clicked);
   };
+
+  const logout = () => {
+    localStorage.clear();
+    navigate('/');
+  }
 
   return (
     <nav>
       <div className="logo">
         <Link className="link" to="/">
-          <img src="https://accexible.com/static/media/accexible_logo.42a306b1.png" alt="logo AcceXible"/>
+          <img
+            src="https://accexible.com/static/media/accexible_logo.42a306b1.png"
+            alt="logo AcceXible"
+          />
         </Link>
       </div>
       <div className="menu-icon" onClick={handleClick}>
         <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
       </div>
-      <ul className={clicked ? "menu-list" : "menu-list close"}>{menuList}</ul>
+      <ul className={clicked ? "menu-list" : "menu-list close"}>
+        {menuList}
+        {userIsLogged ? <button onClick={logout}>cerrar sesión</button> : null}
+      </ul>
     </nav>
   );
 };
